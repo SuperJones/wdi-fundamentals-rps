26936b453784bf9fb300adcbf484bd454313ed50
Anissa Jones <anissajones10@gmail.com>
2016-02-07 14:02:01 -0500

    Going back through functions to test if they work. getPlayMove and getComputerMove work as expected.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -5,7 +5,8 @@
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    var answer = prompt();
+    return answer;
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -23,27 +24,28 @@ function randomPlay() {
 
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
-    var move;
     // If a `move` has a value, your expression should evaluate to that value.
-    if (move == !null){
+    if (move !== null){
         return move;
     }else {
         return getInput();
     }
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
 }
-
+    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
-    var move;
     // If a `move` has a value, your expression should evaluate to that value.
-    if (move == !null){
+    if (move !== null){
         return move;
     }else {
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-     return  randomPlay();/* Your Expression */;
+     return  randomPlay();/* Your Expression */
     } 
 }
+
+//test
+getPlayerMove('scissors');
+getComputerMove('rock');
 
 function getWinner(playerMove,computerMove) {
     var winner;
@@ -51,45 +53,25 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-    if (playerMove == computerMove){
+    if (playerMove === computerMove){
         winner = 'tie';
-    } else if (playerMove == 'rock'){
-        if (computerMove == 'scissors'){
-            winner = 'player';
-        }
-
-    } else if (playerMove == 'paper'){
-        if (computerMove == 'rock'){
-            winner = 'player';
-        }
-
-    } else if (playerMove == 'scissors'){
-        if (computerMove == 'paper')
-            winner = 'player';
-    }else{
-        winner = 'computer';
-    }
-    return winner;
-
-    //ALTERNATE SOLUTION
-    /*
-    if (playerMove == computerMove){
-        winner = 'tie';
-    }else if (getPlayerMove = 'rock' && computerMove == 'scissors') || (getPlayerMove == 'scissors' && computerMove == 'paper') || 
-    (getPlayerMove == 'paper' && computerMove == 'rock'){
+    }else if ((playerMove === 'rock' && computerMove === 'scissors') || (playerMove === 'scissors' && computerMove === 'paper') || (playerMove === 'paper' && computerMove === 'rock')){
         winner = 'player';
     }else{
         winner = 'computer';
     }
     return winner;
-    */
+}
+//test
+getWinner('rock', 'rock');
 
-function playToFive() {
+var playToFive = function() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    getWinner()
     if (getWinner = 'player'){
         playerWins = playerWins + 1
     } else if (getWinner = 'computer'){
---
+++ app/js/testrps.js
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////
+/*   Provided Code - Please Don't Edit   */
+////////////////////////////////////////////////
+'use strict';
+
+function getInput() {
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    var answer = prompt();
+    return answer;
+}
+function randomPlay() {
+    var randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+////////////////////////////////////////////////
+/*           Write Your Code Below            */
+////////////////////////////////////////////////
+
+getInput();
